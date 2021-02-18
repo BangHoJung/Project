9bf210871b9ec208a86fb37f0925f6fc386c51cd
sungyong <asjkvb21@gmail.com>
2021-02-18 11:01:14 +0900

    되라!

--- src/main/java/team/TestClass.java
+++ src/main/java/team/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("yong - write");
+		System.out.println("yong - write");
 		System.out.println("bang - write");
 		System.out.println("nayeon");
 		System.out.println("김형석");
