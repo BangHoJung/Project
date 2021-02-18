99166a6db7aa3767081c6155679c017cb4b182a9
str121 <poilk2447@gmail.com>
2021-02-18 11:43:17 +0900

    333

--- src/main/java/team/TestClass.java
+++ src/main/java/team/TestClass.java
@@ -9,9 +9,9 @@ public class TestClass {
 		System.out.println("nayeon");
 		System.out.println("김형석");
 		System.out.println("동희");
-		System.out.println("동희테스트3");System.out.println("동희테스트444s44");
 		System.out.println("동희테스트3");System.out.println("동희테스트3");
 		System.out.println("이게마지막이길!ㅇ!?!?");
 		System.out.println("nayeon333333");
+	
 	}
 }
