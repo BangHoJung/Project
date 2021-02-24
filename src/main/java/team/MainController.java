e78fe1e22e2d0a6cd7251957fcc584977a2cefa3
ITSC <ITSC@DESKTOP-L7VVF0L>
2021-02-24 12:41:17 +0900

    insertMemberAddress 컨트롤러 추가

--- src/main/java/team/MainController.java
+++ src/main/java/team/MainController.java
@@ -492,5 +492,13 @@ public class MainController {
 		fis.close();
 		bos.close();
 	}
+   @RequestMapping("insertMemberAddressAction.do")
+	public String insertMemberAddressAction(HttpServletRequest request) {
+	   int no = Integer.parseInt(request.getParameter("no"));
+	   System.out.println(no);
+	   String address = request.getParameter("address");
+	   System.out.println(address);
+	   return null;
+   }
 } 
 	   
