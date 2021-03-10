937df612e97f156b085fcd6847ab2aaf034753f3
BangHoJung <35861342+BangHoJung@users.noreply.github.com>
2021-03-10 17:25:28 +0900

    d

--- src/main/java/team/dto/WishDTO.java
+++ src/main/java/team/dto/WishDTO.java
@@ -1,49 +1,6 @@
 package team.dto;
 
 public class WishDTO {
-	private String wish_list_member_id;
-	private String wish_list_store_id;
-	
-
-	public WishDTO(String wish_list_member_id, String wish_list_store_id) {
-		super();
-		this.wish_list_member_id = wish_list_member_id;
-		this.wish_list_store_id = wish_list_store_id;
-	}
-
-
-
-	public String getWish_list_member_id() {
-		return wish_list_member_id;
-	}
-
-
-
-	public void setWish_list_member_id(String wish_list_member_id) {
-		this.wish_list_member_id = wish_list_member_id;
-	}
-
-
-
-	public String getWish_list_store_id() {
-		return wish_list_store_id;
-	}
-
-
-
-	public void setWish_list_store_id(String wish_list_store_id) {
-		this.wish_list_store_id = wish_list_store_id;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "WishlistDTO [wish_list_member_id=" + wish_list_member_id + ", wish_list_store_id=" + wish_list_store_id
-				+ "]";
-	}
-	
-=======
 private String   wish_list_member_id;  
 private String   wish_list_store_id;
 
